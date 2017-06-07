7fa566beaf5509887062f6be873913aaec40e12b
Pierre Fabris <pierref@flux-login3.arc-ts.umich.edu>
2017-06-07 11:35:04 -0400

    Added new class

---
+++ experiment.h
@@ -0,0 +1,19 @@
+#ifndef EXPERIMENT_H
+#define EXPERIMENT_H
+
+#include <iostream>
+#include <>
+
+using namespace std;
+
+class Experiment{
+    public:
+        
+
+    
+    private:
+        
+};
+
+
+#endif
