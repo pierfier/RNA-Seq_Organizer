d422e39bd947eb0817d5cb4d230610615cc233ca
pierfier <fabrispierre@gmail.com>
2017-06-19 10:25:53 -0400

    Working on reading in the data

---
+++ Plotter/Readme.md
@@ -0,0 +1,5 @@
+Program reads in the average data for the experiment and then creates a heatmap
+
+Format for reading data:
+    <header name>   <data name>
+    <gene name> <gene data>
--- Plotter/plot.py
+++ Plotter/plot.py
@@ -1,9 +1,19 @@
-import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 #Read in expression levels with genes
-def readInData(fileName):
+def readInData(fileName, zValues):
+    zValues = []
     print("Reading in data")
+    f = open(fileName, 'r')
+    
+    #Read each line
+    while line = f.readline():
+        numStr = 
+        zValues.append(float(numStr))
 
 #sample heatmap
 def heatMap():
-    
+    trace = go.Heatmap()
+    data = [trace]
+    py.plot(data, "basic-heatmap")
